491523a7bde8507dc9810e7fbe66240682453d98
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2021-11-03 14:43:32 +0200

    Remove pyc.

--- tests/test_train.py
+++ tests/test_train.py
@@ -6,35 +6,18 @@ Verify model before training, for example:
 - Predictions are as expected
 - Trainable variables are updated
 """
-import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
 
 import pytest
 
 from src.fashion.models import get_compiled_model
-from src.fashion.io import get_training_data, label_to_class
+from src.fashion.io import get_training_data
 
 
 def _train(model, train_dataset, val_dataset, epochs=2):
     tf.random.set_seed(33)
     history = model.fit(train_dataset, validation_data=val_dataset, epochs=epochs)
     return history
-
-
-# @pytest.mark.skip(reason="Visualization")
-def test_visualize():
-    train_ds, _ = get_training_data(batch_size=8, train_size=500)
-
-    batch = next(train_ds.take(1).as_numpy_iterator())
-
-    x, y = batch
-    for i in range(x.shape[0]):
-        image = np.reshape(x[i, :], (28, 28))
-        plt.imshow(image, cmap="gray_r")  # type: ignore
-        clazz = label_to_class(y[i][0])
-        plt.title(f"Class: {clazz}")
-        plt.show()
 
 
 @pytest.mark.pretrain
